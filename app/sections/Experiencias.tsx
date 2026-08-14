c75a2bbf0346781d1120476d4e264e0e6c434c57
Gustavo Silveira <gus.nicoletti@gmail.com>
2026-08-14 06:47:31 -0300

    criando tsx de experiencias

---
+++ app/sections/Experiencias.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+const EXPERIENCIAS = [
+    {
+        id: 1,
+        cargo: "Monitor de Programação",
+        empresa: "Infinity School",
+        periodo: "Set 2025 - Dez 2025",
+        descricao: "Aulas particulares e monitoria para alunos, com foco no ensino prático e resolução de problemas em SQL, Python e JavaScript."
+    },
+    {
+        id: 2,
+        cargo: "Tutor de turmas",
+        empresa: "Kodland",
+        periodo: "Jul 2026 - Presente",
+        descricao: "Em construção."
+    },
+]
